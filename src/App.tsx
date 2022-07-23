75d17e43e67ff4eea3747e7cc6c663fb47ce7ea9
Marco Muser <marco.muser@posteo.de>
2022-07-23 10:05:31 +0200

    setup routing and component-library

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import { Landing } from "./pages/landing/Landing";
+import "@shoelace-style/shoelace/dist/themes/light.css";
+import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path";
+
 export const App = () => {
-  return <div className="App"></div>;
+  setBasePath(
+    "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.78/dist/"
+  );
+
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+    </Routes>
+  );
 };
---
+++ src/pages/landing/Landing.tsx
@@ -0,0 +1,5 @@
+import { SlButton } from "@shoelace-style/shoelace/dist/react";
+
+export const Landing = () => {
+  return <SlButton variant="primary">Click me</SlButton>;
+};
